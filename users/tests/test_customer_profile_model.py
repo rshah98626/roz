9abfabf90b371424de9f4bde3882948624bfd5fc
Rahul Shah <rshah98626@gmail.com>
2020-06-30 19:53:04 -0500

    Testing something new

--- users/tests/test_customer_profile_model.py
+++ users/tests/test_customer_profile_model.py
@@ -4,25 +4,30 @@ from ..models import CustomerProfile, Account
 
 class CustomerProfileModelTest(TestCase):
     def setUp(self):
-        CustomerProfile.objects.create(
-            cents=100,
+        self.account_username = 'dummy'
+        self.account_email = 'test@test.com'
+        self.account_first_name = 'Joe'
+        self.account_last_name = 'Blow'
+        self.cents = 100
+
+        self.customer_profile = CustomerProfile.objects.create(
+            cents=self.cents,
             user=Account.objects.create(
-                email='test@test.com',
-                username='dummy',
-                first_name='Joe',
-                last_name='Blow'
+                email=self.account_email,
+                username=self.account_username,
+                first_name=self.account_first_name,
+                last_name=self.account_last_name
             )
         )
 
-    def test_customer_profile_has_correct_user(self):
-        profile = CustomerProfile.objects.get(cents=100)
-        account = Account.objects.get(username='dummy')
-        self.assertEqual(profile.user, account)
+    def test_customer_profile_has_correct_user_information(self):
+        self.assertEqual(self.customer_profile.user.email, self.account_email)
+        self.assertEqual(self.customer_profile.user.first_name, self.account_first_name)
+        self.assertEqual(self.customer_profile.user.last_name, self.account_last_name)
+        self.assertEqual(self.customer_profile.user.username, self.account_username)
 
     def test_customer_profile_has_correct_cents(self):
-        profile = CustomerProfile.objects.get(cents=100)
-        self.assertEqual(profile.cents, 100)
+        self.assertEqual(self.customer_profile.cents, self.cents)
 
     def test_customer_profile_get_dollars(self):
-        profile = CustomerProfile.objects.get(cents=100)
-        self.assertEqual(profile.get_dollars, 1)
+        self.assertEqual(self.customer_profile.get_dollars, self.cents / 100)
